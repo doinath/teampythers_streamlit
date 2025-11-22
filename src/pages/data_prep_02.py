a0666d36bd8215bdbc72a19288554965b9262fb5
doinath <nathanaeljedd.delcastillo@cit.edu>
2025-11-22 23:46:04 +0800

    added pages directory and .py files: overview_01.py, data_prep_02.py

---
+++ src/pages/data_prep_02.py
@@ -0,0 +1,7 @@
+
+class DataPrep:
+    def __init__(self, data_manager):
+        self.dm = data_manager
+        
+    def render(self):
+        pass
---
+++ src/pages/overview_01.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+
+class OverviewPage:
+    def __init__(self, data_manager):
+            self.dm = data_manager
+    
+    def render(self):
+        st.header('01 Overview Section')
+        
+
+    # TODO: Add visualizations for distributions and missing data.
+        pass
+        
+    # TODO: Use st.expander() to detail cleaning steps.
+        pass
